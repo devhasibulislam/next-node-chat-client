00fe0bf4ec839b3bf55abe1d83b2c1bc17b3b3f4
hasibulislam999 <hasib143sl@gmail.com>
2022-09-07 00:37:48 +0600

    chat index created

---
+++ pages/chat/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from '../../components/Header';
+
+const Chat = () => {
+    return (
+        <section>
+            <Header title="Conversations" />
+            <Layout>
+            </Layout>
+        </section>
+    );
+}
+export default Chat;
