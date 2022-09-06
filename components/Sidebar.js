f60a2bc190d566da28bd2d0f7cf9743da866a6b8
hasibulislam999 <hasib143sl@gmail.com>
2022-09-06 22:20:17 +0600

    sidebar modified

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
-import styles from '../styles/Layout.module.css';
+import styles from '../styles/Sidebar.module.css';
 import { Nav } from 'react-bootstrap';
 import Image from 'next/image';
 
@@ -35,8 +35,8 @@ const Sidebar = () => {
                                         role="button"
                                     />
                                     <p className={`mb-0 ${styles.visibility}`}> • {dta.first_name} • </p>
-                                    <p className={`${styles.removeUser} shadow d-flex justify-content-center align-items-center bg-white rounded-circle`} role="button">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="">
+                                    <p className={`${styles.removeUser} shadow d-flex justify-content-center align-items-center bg-white rounded-circle mb-0`} role="button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
                                         </svg>
                                     </p>
@@ -45,6 +45,18 @@ const Sidebar = () => {
                         }
                     </Nav>
             }
+            <div
+                className='d-flex align-items-center justify-content-center position-absolute bottom-0 start-0 w-100 py-3 rounded-end text-white'
+                style={{backgroundColor: "#808080"}}
+                role="button"
+            >
+                <p className={`${styles.addUser} bg-white text-black rounded-circle mb-0`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                    </svg>
+                </p>
+                <p className={`${styles.visibility} mb-0 ms-3 fs-5 fw-normal font-monospace`}>Add Conversation</p>
+            </div>
         </section>
     );
 }
