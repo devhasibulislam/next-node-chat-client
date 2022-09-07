99c3018195db497dc4cafb248d7975e104caff20
hasibulislam999 <hasib143sl@gmail.com>
2022-09-07 08:38:01 +0600

    sidebar modified

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,6 +5,7 @@ import { Nav } from 'react-bootstrap';
 import Image from 'next/image';
 import Window from './Window';
 import Users from './Users';
+import Link from 'next/link';
 
 const Sidebar = () => {
     const [data, setData] = useState(null);
@@ -27,17 +28,20 @@ const Sidebar = () => {
                 loading ? <Loading /> :
                     <Nav className={`flex-column`}>
                         {
-                            data?.map((dta, index) => <div key={dta._id} className={`${index !== (data.length - 1) && "mb-3"} px-3 py-1 ${styles.userSelect}`}>
+                            data?.map((dta, index) => <div key={dta._id} className={`${index !== (data.length - 1) && "mb-3"} ${index === (data.length - 1) && "mb-5"} px-3 py-1 ${styles.userSelect}`}>
                                 <div className={`d-flex justify-content-between align-items-center ${styles.userStack}`}>
-                                    <Image
-                                        src={dta.avatar}
-                                        alt={`dta.last_name`}
-                                        height={50}
-                                        width={50}
-                                        className={`${styles.chatAvatar} rounded-circle`}
-                                        role="button"
-                                    />
-                                    <p className={`mb-0 ${styles.visibility}`}> • {dta.first_name} • </p>
+                                    <Link href={`/chats/${dta._id}`}>
+                                        <a className={`${styles.anchor} ${styles.userStack} d-flex justify-content-between align-items-center`}>
+                                            <Image
+                                                src={dta.avatar}
+                                                alt={`dta.last_name`}
+                                                height={50}
+                                                width={50}
+                                                className={`${styles.chatAvatar} rounded-circle`}
+                                            />
+                                            <p className={`mb-0 ${styles.visibility}`}> • {dta.first_name} • </p>
+                                        </a>
+                                    </Link>
                                     <p className={`${styles.removeUser} shadow d-flex justify-content-center align-items-center bg-white rounded-circle mb-0`} role="button">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
