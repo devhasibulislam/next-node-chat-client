bfcb61777fc9e1bb1f170d0ad6d265d38fcccb71
hasibulislam999 <hasib143sl@gmail.com>
2022-09-08 15:53:13 +0600

    useUserData modified

---
+++ hooks/useUserData.js
@@ -0,0 +1,23 @@
+import useSWR from 'swr';
+
+function useUserData() {
+    const fetcher = (...args) => fetch(...args).then(res => res.json());
+    const { data, error } = useSWR("http://localhost:5000/users", fetcher, {
+        revalidateOnFocus: true,
+        refreshWhenHidden: true,
+        refreshWhenOffline: true,
+        shouldRetryOnError: true,
+        refreshInterval: 1000
+    });
+
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+
+    return {
+        data: data.data,
+        loading: !error && !data,
+        isError: error
+    }
+}
+
+export default useUserData;
