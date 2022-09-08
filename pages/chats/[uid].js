dff232a3bf5a0d4de769f7d3c0408f6111d0bde1
hasibulislam999 <hasib143sl@gmail.com>
2022-09-08 15:53:19 +0600

    uid modified

--- pages/chats/[uid].js
+++ pages/chats/[uid].js
@@ -1,26 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Layout from '../../components/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from '../../components/Header';
 import { useRouter } from 'next/router';
 import User from '../../components/User';
 import Loading from '../../components/Loading';
+import useUserData from '../../hooks/useUserData';
 
 const UserID = () => {
     const router = useRouter();
     const { uid } = router.query;
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setLoading(true);
-        fetch('/api/users')
-            .then(request => request.json())
-            .then(response => {
-                setData(response.data);
-                setLoading(false);
-            }).catch(error => console.log(error.message));
-    }, []);
+    const { data, loading } = useUserData();
 
     const matchedData = data?.find(dta => dta._id === Number(uid));
 
