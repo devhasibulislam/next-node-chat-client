8df9d988a35661287bcc4b3ba03986e7b59c7488
hasibulislam999 <hasib143sl@gmail.com>
2022-09-08 15:53:05 +0600

    users modified

--- components/Users.js
+++ components/Users.js
@@ -3,21 +3,12 @@ import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
 import styles from '../styles/Users.module.css';
 import Link from 'next/link';
+import useUserData from '../hooks/useUserData';
 
 const Users = (props) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
     const [keyword, setKeyword] = useState("");
 
-    useEffect(() => {
-        setLoading(true);
-        fetch('/api/users')
-            .then(request => request.json())
-            .then(response => {
-                setData(response.data);
-                setLoading(false);
-            });
-    }, []);
+    const {data, loading} = useUserData();
 
     // RegEx
     const condition = new RegExp(keyword, "gi");
@@ -44,16 +35,17 @@ const Users = (props) => {
                                     onClick={props.onHide}
                                 >
                                     <Link href={`/chats/${dta._id}`}>
-                                        <a className={`d-flex justify-content-between align-items-center ${index !== (data.length - 1) && "mb-2"} ${styles.stackUsers} py-1 me-2 ${styles.anchor}`}>
-                                            <div className='d-flex align-items-center'>
-                                                <Image
-                                                    src={dta.avatar}
-                                                    alt={`dta.last_name`}
-                                                    height={50}
-                                                    width={50}
-                                                    className={`${styles.chatAvatar} rounded-circle`}
-                                                    role="button"
-                                                />
+                                        <a className={`d-flex justify-content-between align-items-center ${index !== (data.length - 1) && "mb-2"} ${styles.stackUsers} py-1 me-2 ps-2 ${styles.anchor}`}>
+                                            <div className='d-flex align-items-center' style={{columnGap: ".5rem"}}>
+                                                <span style={{
+                                                    height: "25px",
+                                                    width: "25px"
+                                                }}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                    </svg>
+
+                                                </span>
                                                 <p className='mb-0'>{dta.first_name} {dta.last_name}</p>
                                             </div>
                                             <p className={`${styles.user} mb-0`}>
